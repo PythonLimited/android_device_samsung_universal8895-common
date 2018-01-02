c7a79dccdd9859c7712750ce84ff24bdbf58fdf4
Vaughnellope <michaelbt@live.com>
2018-01-03 07:32:11 +1100

    sepolicy rework

--- universal8895-common.mk
+++ universal8895-common.mk
@@ -147,8 +147,6 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
 	camera.exynos5 \
-	libcamhelpr \
-        shim_camera \
 	camera.device@3.2-impl \
 	camera.device@1.0-impl \
 	android.hardware.camera.provider@2.4-impl
